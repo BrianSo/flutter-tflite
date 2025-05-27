fa8170507b84a23e1abfa2cb046b466902c4a316
BrianSo <briansostudio@gmail.com>
2025-05-27 21:40:31 +0800

    Fix compilation issue for flutter 3.22

--- lib/src/tensor.dart
+++ lib/src/tensor.dart
@@ -50,8 +50,7 @@ class Tensor {
   /// Underlying data buffer as bytes.
   Uint8List get data {
     final data = cast<Uint8>(tfLiteTensorData(_tensor));
-    return UnmodifiableUint8ListView(
-        data.asTypedList(tfLiteTensorByteSize(_tensor)));
+    return data.asTypedList(tfLiteTensorByteSize(_tensor)).asUnmodifiableView();
   }
 
   /// Quantization Params associated with the model, [only Android]
